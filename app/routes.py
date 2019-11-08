952bd63b55dfc4d2340eb2febd650e2b6c283d6d
vinodhreddyt <vtanigun@kent.edu>
2019-11-07 23:03:28 -0500

    changes

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,14 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
+     <html>
+        <head>
+            <title>Home Page - Microblog</title>
+        </head>
+        <body>
+            <h1>Hello, ''' + user['username'] + '''!</h1>
+        </body>
+    </html>
     user = {'username': 'crazy'}
     posts = [
         {
@@ -16,11 +24,4 @@ def index():
         }
     ]
     return  render_template('index.html', title='Home', user=user,posts=posts)
-<html>
-    <head>
-        <title>Home Page - Microblog</title>
-    </head>
-    <body>
-        <h1>Hello, ''' + user['username'] + '''!</h1>
-    </body>
-</html>'''
+    
