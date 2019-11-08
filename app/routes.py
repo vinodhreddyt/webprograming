dcd825c43d0ca7986f27ddbaedf54007d5d93497
vinodhreddyt <vtanigun@kent.edu>
2019-11-07 22:07:41 -0500

    change

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,21 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'crazy'}
-    return '''
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return  render_template('index.html', title='Home', user=user,posts=posts)
 <html>
     <head>
         <title>Home Page - Microblog</title>
