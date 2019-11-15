4dd28c8dc5ab94d75dda1d1420ab34a476456602
vinodhreddyt <vtanigun@kent.edu>
2019-11-14 22:09:56 -0500

    dd

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,19 @@
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'chapo'}
-    return '''
-                <html>
-                    <head>
-                        <title>Home Page - Microblog</title>
-                    </head>
-                    <body>
-                        <h1>Hello, ''' + user['username'] + '''!</h1>
-                    </body>
-                </html>'''
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
