59c5ccf45149287bca79fde98f93cf66cdd7a3f7
vinodhreddyt <vtanigun@kent.edu>
2019-11-14 21:43:37 -0500

    jj

--- app/routes.py
+++ app/routes.py
@@ -5,11 +5,11 @@ from app import app
 def index():
    user = {'username': 'chapo'}
     return '''
-<html>
-    <head>
-        <title>Home Page - Microblog</title>
-    </head>
-    <body>
-        <h1>Hello, ''' + user['username'] + '''!</h1>
-    </body>
-</html>'''
+                <html>
+                    <head>
+                        <title>Home Page - Microblog</title>
+                    </head>
+                    <body>
+                        <h1>Hello, ''' + user['username'] + '''!</h1>
+                    </body>
+                </html>'''
